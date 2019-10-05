29814a9ad4303c591d989f431b1895cfb58bb4f9
ridhaC <RidhaC11@hotmail.com>
2019-10-05 02:10:25 -0400

    Food Generated By Center

--- src/ship/CenterModule.java
+++ src/ship/CenterModule.java
@@ -7,13 +7,14 @@ import main.SpriteCodex;
 public class CenterModule extends ShipModule {
 
 	public CenterModule(Ship inShip) {
-		super(inShip, 0, 1.0f);
+		super(inShip, 0, 10.0f);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void generateResource() {
 		this.getShip().addPower(3.0f);
+		this.getShip().addFood(1.0f);
 	}
 
 	@Override
